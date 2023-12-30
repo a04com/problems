915fa00fb5d668cd58a4c5c4ab2e3c22102c2014
Ali <alasylkhh@gmail.com>
2023-12-30 13:30:31 +0600

    solution

--- 704 binary.py
+++ 704 binary.py
@@ -1,1 +1,17 @@
-print("Hello world")
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        mid = (low + high) // 2
+
+        while high >= low:
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+                mid = (low + high) // 2
+            elif nums[mid] > target:
+                high = mid - 1
+                mid = (low + high) // 2
+        return -1
+
+print(Solution().search([-5], 5))
